52a540c6f1af5258650e87eb54060af97c93b312
kush bhardwaj <kush@qurilo.com>
2025-07-30 18:22:35 +0530

    Removed unnecessary lines and added newline at end of file

--- src/utilitis/cloudinary.js
+++ src/utilitis/cloudinary.js
@@ -5,8 +5,5 @@ cloudinary.config({
     cloud_name:process.env.CloudName,
     api_key:process.env.CloudinaryApiKey,
     api_secret:process.env.CloudinarySecretKey
-})
-
-
-
+});
 module.exports =  cloudinary;
