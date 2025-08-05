4d1dd58876a8f97d5b14e680f2509f4ee5774356
kush bhardwaj <kush@qurilo.com>
2025-08-05 15:57:45 +0530

    invoice compelte

--- src/controllers/invoce/insertInvoiceController.js
+++ src/controllers/invoce/insertInvoiceController.js
@@ -6,6 +6,7 @@ const pdfParse = require('pdf-parse');
 const cloudinary = require('../../utilitis/cloudinary');
 const superbase = require('../../config/superbase storage/superbaseConfig');
 const InvoiceModel = require('../../models/InvoiceModel');
+const { default: mongoose } = require('mongoose');
 
 exports.viewInvoice = async (req, res, next) => {
     try {
@@ -24,7 +25,7 @@ exports.downloadInvoice = async (req, res, next) => {
 // test
 exports.uploadInvoice = async (req, res, next) => {
     try {
-        const {user_id} = req.params;
+        const { user_id } = req.params;
         const file = req.file;
         if (!file) {
             return res.status(400).json({ message: "No file uploaded" });
@@ -71,26 +72,26 @@ exports.uploadInvoice = async (req, res, next) => {
                 ? `Monthly Subscription Fees (${extract(/Monthly Subscription Fees\s*\n*\(([^)]+)\)/i)})`
                 : "Monthly Subscription Fees",
             totalAmount: extract(/Total Amount\s+₹?\s*([\d,]+)/i),
-            url:publicUrl,
+            url: publicUrl,
             user_id
         };
 
 
         fs.unlinkSync(filePath);
-       
+
         const result = await InvoiceModel.create(invoiceData);
-        if(!result){
-            return res.status(500).json({message: "Failed to upload invoice"});
+        if (!result) {
+            return res.status(500).json({ message: "Failed to upload invoice" });
         }
         return res.status(200).json({
             message: "PDF uploaded",
-            success:true
+            success: true
         });
 
     } catch (err) {
         console.error("Error processing invoice:", err);
         return res.status(500).json({
-            success:false,
+            success: false,
             message: "Internal Server Error",
             error: err.message
         });
@@ -98,43 +99,54 @@ exports.uploadInvoice = async (req, res, next) => {
 };
 
 // get all invoices for user
-exports.getInvoices = async (req , res, next) => {
-    try{
-        const {user_id} = req.params;
-        if(!user_id){
-            return res.status(400).json({message: "User required"});
-        } 
-        const result = await InvoiceModel.find({user_id});
-        if(!result){
-            return res.status(404).json({message: "No invoices found"});
+exports.getInvoices = async (req, res, next) => {
+    try {
+        const { user_id } = req.params;
+        if (!user_id) {
+            return res.status(400).json({ message: "User required" });
         }
-        return res.status(200).json({success:true, data});
-    }catch(error){
+        const result = await InvoiceModel.find({ user_id });
+        if (!result) {
+            return res.status(404).json({ message: "No invoices found" });
+        }
+        return res.status(200).json({ success: true, data });
+    } catch (error) {
         return res.status(500).json({
-            success:false,
+            success: false,
             message: error.message
-            });
+        });
     }
 }
 
 
 // get invoice by date
 exports.getInvoicesByMonthYear = async (req, res) => {
-  try {
-    const { month, year } = req.query;
-    console.log(month,year)
-    if (!month || !year) {
-      return res.status(400).json({ message: "Month and year required" });
+    try {
+        const { month, year, id } = req.query;
+        if (!month || !year) {
+            return res.status(400).json({ message: "Month and year required" });
+        }
+        const isId = mongoose.Types.ObjectId.isValid(id)
+        if(!isId){
+            return res.status(400)
+            .json({success:false,message:"Invalid id"})
+        }
+        const formattedMonth = month.padStart(2, '0');
+        const regex = new RegExp(`/${formattedMonth}/${year}$`);
+
+        const invoices = await InvoiceModel.find(
+            {
+                user_id: id,
+                invoiceDate: {
+                    $regex: regex
+                }
+            });
+        if(invoices.length ===0){
+            return res.status(404)
+            .json({message:"no invoice found",success:false})
+        }
+        res.status(200).json({ success: true, count: invoices.length, data: invoices });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    const formattedMonth = month.padStart(2, '0');
-    const regex = new RegExp(`/${formattedMonth}/${year}$`);
-
-    const invoices = await InvoiceModel.find({
-      invoiceDate: { $regex: regex }
-    });
-
-    res.status(200).json({ success: true, count: invoices.length, data: invoices });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
