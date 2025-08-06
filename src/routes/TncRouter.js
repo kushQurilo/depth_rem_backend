ae889b1e35622d1115120b3acb52f2c59d8779d2
kush bhardwaj <kush@qurilo.com>
2025-08-06 12:06:46 +0530

    tnc all compelete

--- src/routes/TncRouter.js
+++ src/routes/TncRouter.js
@@ -1,5 +1,7 @@
 const TncRoutetr = require('express').Router();
-const { addTNC } = require('../controllers/termsAndConditionController');
+const { addTNC, updateTnc, deleteTnc } = require('../controllers/termsAndConditionController');
 TncRoutetr.post('/add',addTNC);
+TncRoutetr.put('/update',updateTnc);
+TncRoutetr.delete('/delete',deleteTnc);
 
 module.exports = TncRoutetr
